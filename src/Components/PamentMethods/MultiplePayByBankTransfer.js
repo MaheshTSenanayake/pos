2246b895cb77d9898bb1434f8b84b2b637766cb8
mahrshSenanayake <mahesh.tharanga92@gmail.com>
2023-05-20 01:33:09 +0530

    Complete multiple payment advance and bank transfer methods

--- src/Components/PamentMethods/MultiplePayByBankTransfer.js
+++ src/Components/PamentMethods/MultiplePayByBankTransfer.js
@@ -1,19 +1,59 @@
-import { Grid, TextField } from "@mui/material";
+import { Button, Grid, TextField } from "@mui/material";
+import { useState } from "react";
 
-const MultiplePayByBankTransfer = () => {
+const MultiplePayByBankTransfer = (props) => {
+  const [bankTransferData, setBankTransferData] = useState({});
+  const saveAdvancePaymetData = () => {
+    props.sendAdvanceData(bankTransferData);
+  };
+  const bankAccountNumberHandler = (e) => {
+    setBankTransferData({
+      ...bankTransferData,
+      methodDetails: { accountNumber: e.target.value },
+      methodListKey: props.methodKey,
+    });
+  };
+  const paymentNoteHandler = (e) => {
+    setBankTransferData({
+      ...bankTransferData,
+      methodDetails: {
+        ...bankTransferData.methodDetails,
+        paymentNote: e.target.value,
+      },
+    });
+  };
+
   return (
     <Grid container>
       <Grid item xs={10} marginTop="10px">
-        <TextField variant="outlined" label="Bank Account Number" fullWidth />
-      </Grid>
-      <Grid item xs={10} marginTop="10px">
         <TextField
-          multiline
-          rows={4}
+          onChange={bankAccountNumberHandler}
           variant="outlined"
           label="Bank Account Number"
           fullWidth
         />
+      </Grid>
+      <Grid item xs={10} marginTop="10px">
+        <TextField
+          onChange={paymentNoteHandler}
+          multiline
+          rows={4}
+          variant="outlined"
+          label="Payment Note"
+          fullWidth
+        />
+        <Button
+          sx={{
+            textAlign: "center",
+            bgcolor: "#05abeb",
+            borderRadius: 1,
+            margin: 1,
+          }}
+          variant="contained"
+          onClick={saveAdvancePaymetData}
+        >
+          Save
+        </Button>
       </Grid>
     </Grid>
   );
